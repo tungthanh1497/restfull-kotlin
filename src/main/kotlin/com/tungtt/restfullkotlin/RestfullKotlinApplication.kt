fedbf2eb711549371becbe015ba934d05fe8c764
tungtt <tungthanh1497@gmail.com>
2021-02-08 09:08:17 +0700

    Initial commit

---
+++ src/main/kotlin/com/tungtt/restfullkotlin/RestfullKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.tungtt.restfullkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfullKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestfullKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/tungtt/restfullkotlin/RestfullKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tungtt.restfullkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestfullKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
